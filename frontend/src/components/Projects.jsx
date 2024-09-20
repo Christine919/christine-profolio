1faa9c8a4bef0a15c3b46cfc36f8352acfb1a9b1
Christine <161996117+Christine919@users.noreply.github.com>
2024-09-21 00:34:40 +0800

    updated

--- frontend/src/components/Projects.jsx
+++ frontend/src/components/Projects.jsx
@@ -5,6 +5,7 @@ import ProjectCard from './ProjectCard';
 import project_1 from '../assets/project_1.png';
 import project_2 from '../assets/project_2.png';
 import project_3 from '../assets/project_3.png';
+import project_4 from '../assets/project_4.png';
 
 const projects = [
   {
